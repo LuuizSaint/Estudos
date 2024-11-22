78e8daf54effaafff173c22b10fd84ff00f34c92
Luiz R. <luizroberto44630@gmail.com>
2024-11-22 10:47:40 -0300

    Class Response

--- mvc2/app/http/Response.php
+++ mvc2/app/http/Response.php
@@ -2,4 +2,29 @@
 
 namespace app\http;
 
-class Response {}
+class Response
+{
+    private int $httpCode = 200;
+    private array $headers = [];
+    private string $contentType = "text/html";
+    private mixed $content;
+
+
+    public function __construct(int $httpCode, mixed $content, string $contentType = "text/html")
+    {
+        $this->httpCode = $httpCode;
+        $this->content = $content;
+        $this->setContentType($contentType);
+    }
+
+    public function setContentType(string $contentType)
+    {
+        $this->contentType = $contentType;
+        $this->addHeader("ContentType", $contentType);
+    }
+
+    public function addHeader(string $key, string $value)
+    {
+        $this->headers[$key] = $value;
+    }
+}
