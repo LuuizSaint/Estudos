cfdd3204b2420c1a4fbe53da3509b1308d29c053
Luu1zSaint <luizroberto44630@gmail.com>
2025-01-29 16:18:37 -0300

    JS Study

--- DOM/assets/js/script.js
+++ DOM/assets/js/script.js
@@ -1,5 +1,7 @@
-sudo dpkg -i ./containerd.io_stable_amd64.deb \
-  ./docker-ce_stable_amd64.deb \
-  ./docker-ce-cli_stable_amd64.deb \
-  ./docker-buildx-plugin_stable_amd64.deb \
-  ./docker-compose-plugin_stable_amd64.deb
+CREATE DATABASE meu_banco_de_dados;
+SHOW DATABASES;
+
+CREATE USER 'usuario_teste'@'localhost' IDENTIFIED BY 'senha123';
+GRANT ALL PRIVILEGES ON meu_banco_de_dados.* TO 'usuario_teste'@'localhost';
+FLUSH PRIVILEGES;
+
