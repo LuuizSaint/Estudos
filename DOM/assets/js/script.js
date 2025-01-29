4c316d081e6de60daefad9d0d90d9d88c5867b40
Luu1zSaint <luizroberto44630@gmail.com>
2025-01-29 09:56:06 -0300

    Arrays

---
+++ DOM/assets/js/script.js
@@ -0,0 +1,5 @@
+sudo dpkg -i ./containerd.io_stable_amd64.deb \
+  ./docker-ce_stable_amd64.deb \
+  ./docker-ce-cli_stable_amd64.deb \
+  ./docker-buildx-plugin_stable_amd64.deb \
+  ./docker-compose-plugin_stable_amd64.deb
