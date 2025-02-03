d4f5e1ad7a03c0a631f7e363ea0acf981cc14c68
Luu1zSaint <luizroberto44630@gmail.com>
2025-02-03 10:04:07 -0300

    Udemy Array

--- phpUdemy/array/index.php
+++ phpUdemy/array/index.php
@@ -70,28 +70,69 @@ $ipva = true;
 $carro = compact("marca", "modelo", "cor", "portas", "ipva");
 
 
-$arrReduce = [1, 5, 23, 4, 567, 33, 23, 56, 0, 98, 123];
+// in_array(item, arr) Retona um bool caso encontre o valor passano no array
+in_array("RS6", $carro); //Out: bool(true) 
+in_array("Jorge", $carro); //Out: bool(false) 
 
+
+$arrReduce = [1, 5, 23, 4, 567, 33, 23, 56, 0, 98, 123];
+// array_reduce(arr, callback) Reduz o array em apenas um valor
 $result = array_reduce($arrReduce, function($a, $b) {
     return $a + $b;
 });
 
-var_dump($result);
+$arrUnique = [1, 2, 2, 3, 4, 4];
+// array_unique(arr) para remover valores duplicados
+$uni = array_unique($arrUnique);
+$uni; // [1, 2, 3, 4]
+
+$arrMap = [1, 2, 3, 4];
+// array_map(callback, arr) Realiza uma operação em cada elmnt do array
+$doble = array_map(fn($n) => $n * 2, $arrMap);
+$doble; // [2, 4, 6, 8]
+
+sort($arrReduce); //Ordena o array em ordem crescente
+rsort($arrSlice); //Ordena o array em ordem decrescente
+$arrReduce; //Out: Array ( [0] => 0 [1] => 1 [2] => 4 [3] => 5 [4] => 23 [5] => 23 [6] => 33 [7] => 56 [8] => 98 [9] => 123 [10] => 567 ) 
+$arrSlice; //Out: Array ( [0] => Perinha [1] => Naninha [2] => Muranguin [3] => Bacaxi [4] => Bacatizin ) 
+
+$arrSort = [
+    "Pedrin" => 23,
+    "Robertin" => 2,
+    "Duardin" => 14,
+    "Guizin" => 7,
+    "Junin" => 20
+
+];
+
+asort($arrSort); //Ordena arrays associativos pelo value
+// arsort(arr) Ordena decrescente
+$arrSort; //Out: Array ( [Robertin] => 2 [Guizin] => 7 [Duardin] => 14 [Junin] => 20 [Pedrin] => 23 ) 
+
+ksort($arrSort); //Ordena arrays associativos pela key
+// krsort(arr) Ordena decrescente
+$arrSort; //Out: Array ( [Duardin] => 14 [Guizin] => 7 [Junin] => 20 [Pedrin] => 23 [Robertin] => 2 ) 
 
 
+$arrReverse = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// array_reverse(arr) Inverte a ordem do array
+$newArrReverse = array_reverse($arrReverse);
+$newArrReverse; //Out: Array ( [0] => 10 [1] => 9 [2] => 8 [3] => 7 [4] => 6 [5] => 5 [6] => 4 [7] => 3 [8] => 2 [9] => 1 ) 
+
+$arrShuffle = $arrReverse;
+// shuffle(arr) Ordena de forma aleatoria o array
+shuffle($arrShuffle);
+$arrShuffle; //Out: Array ( [0] => 7 [1] => 10 [2] => 5 [3] => 3 [4] => 1 [5] => 8 [6] => 9 [7] => 6 [8] => 4 [9] => 2 ) 
 
 
+$arrSum = $arrReverse;
+// array_sum(arr) Soma todos os items do array
+$sumArr = array_sum($arrSum);
+$sumArr; //Out: 55
 
-
-
-
-
-
-
-
-
-
-
+// array_merge(arr, arr..., arr) Uni arrays 
+$arrMerge = array_merge($arrReverse, $arrSlice);
+$arrMerge; //Out: Array ( [0] => 1 [1] => 2 [2] => 3 [3] => 4 [4] => 5 [5] => 6 [6] => 7 [7] => 8 [8] => 9 [9] => 10 [10] => Perinha [11] => Naninha [12] => Muranguin [13] => Bacaxi [14] => Bacatizin ) 
 
 $arrPerson = [
     "Pedrin" => 23,
